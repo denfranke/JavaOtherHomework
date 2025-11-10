9aa0dbca06d9b2886dd429475bee6074e0364030
Denis <33222627+denfranke@users.noreply.github.com>
2025-11-10 10:57:02 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,71 +1,55 @@
 package OtherHomework;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
-        compare(1);
-        compare(2);
-        compare(5);
-        compare(15);
+    public static void main(String[] args) throws IOException {
+        int[][] teams = {
+                {45, 31, 24, 22, 20, 17, 14, 13, 12, 10},
+                {31, 18, 15, 12, 10, 8, 6, 4, 2, 1},
+                {51, 30, 10, 9, 8, 7, 6, 5, 2, 1}
+        };
+
+        int[] nationalTeam = mergeAll(teams);
+        System.out.println(Arrays.toString(nationalTeam));// [51, 45, 31, 31, 30, 24, 22, 20, 18, 17]
     }
 
-    public static void compare(int day) {
-        System.out.println("=== Day " + day + " ===");
-        int[] startNumbers = {21, 1, 20, 23};
-        int iterative = chooseHobbyIterative(startNumbers, day);
-        int recursive = chooseHobbyRecursive(startNumbers, day, new int[day]);
-        System.out.println("Iterative = " + iterative + " | Recursive = " + recursive);
-        System.out.println();
-    }
-
-    public static int chooseHobbyRecursive(int[] startNumbers, int day, int[] memory)
-    {
-        if (memory[day - 1] != 0)
-            return memory[day - 1];
-        else {
-            int day1 = (startNumbers[startNumbers.length - 1] * startNumbers[startNumbers.length - 3]) % 10 + 1;
-            if (day == 1) return day1;
-
-            int day2 = (day1 * startNumbers[startNumbers.length - 2]) % 10 + 1;
-            if (day == 2) return day2;
-
-            int day3 = (day2 * startNumbers[startNumbers.length - 1]) % 10 + 1;
-            if (day == 3) return day3;
-
-            int day4 = (day3 * day1) % 10 + 1;
-            if (day == 4) return day4;
-
-            memory[0] = day1;
-            memory[1] = day2;
-            memory[2] = day3;
-            memory[3] = day4;
-            
-            int prev = chooseHobbyRecursive(startNumbers, day - 1, memory);
-            int prevPrePrev = chooseHobbyRecursive(startNumbers, day - 3, memory);
-            
-            memory[day - 1] = (prev * prevPrePrev) % 10 + 1;
-            return (prev * prevPrePrev) % 10 + 1;
+    /**
+     * Метод для слияния всех команд в одну национальную
+     */
+    public static int[] mergeAll(int[][] teams) {
+        int[] ans = teams[0];
+        for (int i = 1; i < teams.length; i++) {
+            ans = merge(ans, teams[i]);
         }
+        return ans;
     }
 
-    public static int chooseHobbyIterative(int[] startNumbers, int day) {
-        List<Integer> numbers = new ArrayList<>();
+    /**
+     * Метод для слияния двух команд в одну
+     */
+    public static int[] merge(int[] teamA, int[] teamB) {
+        int[] c = new int[10];
+        int ia = 0, ib = 0, ic = 0;
 
-        numbers.add(startNumbers[0]);
-        numbers.add(startNumbers[1]);
-        numbers.add(startNumbers[2]);
-        numbers.add(startNumbers[3]);
-
-        for (int d = 0; d < day; d++) {
-            int index = d + 4; // индексы дней в массиве сдвинуты на 4
-            int prev = numbers.get(index - 1); // предыдущее значение
-            int prePrePrev = numbers.get(index - 3); // пре-пре-предыдущее значение
-            numbers.add((prev * prePrePrev) % 10 + 1);
+        while ((ia < teamA.length || ib < teamB.length) && ic < 10) {
+            if (ia == teamA.length) {
+                c[ic] = teamB[ib];
+                ib++;
+            } else if (ib == teamB.length) {
+                c[ic] = teamA[ia];
+                ia++;
+            } else if (teamA[ia] >= teamB[ib]) {
+                c[ic] = teamA[ia];
+                ia++;
+            } else {
+                c[ic] = teamB[ib];
+                ib++;
+            }
+            ic++;
         }
-
-        return numbers.get(numbers.size() - 1);
+        return c;
     }
 }
